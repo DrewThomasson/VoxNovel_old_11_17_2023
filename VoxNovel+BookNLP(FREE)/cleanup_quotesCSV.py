28fbec875c1fe5d3f6d0797bee360fc9ca3f9500
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2023-10-08 01:08:06 -0400

    Update cleanup_quotesCSV.py

--- VoxNovel+BookNLP(FREE)/cleanup_quotesCSV.py
+++ VoxNovel+BookNLP(FREE)/cleanup_quotesCSV.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import nltk
+import re
 
 # Ensure the necessary NLTK data is downloaded
 nltk.download('averaged_perceptron_tagger')
@@ -9,8 +10,15 @@ def is_pronoun(word):
     tag = nltk.pos_tag([word])[0][1]
     return tag in ["PRP", "PRP$", "WP", "WP$"]
 
+def has_alphanumeric(text):
+    """Checks if the given text contains any alphanumeric characters."""
+    return bool(re.search(r'\w', text))
+
 # Load the quotes.csv file into a DataFrame
 df = pd.read_csv("quotes.csv")
+
+# Remove rows where the "Text" column doesn't contain any alphanumeric characters
+df = df[df["Text"].apply(has_alphanumeric)]
 
 # Replace speaker names that match the format "{CharID}:{pronoun}" with "Narrator"
 for index, row in df.iterrows():
@@ -22,4 +30,3 @@ for index, row in df.iterrows():
 
 # Save the updated DataFrame back to quotes.csv
 df.to_csv("quotes.csv", index=False)
-
