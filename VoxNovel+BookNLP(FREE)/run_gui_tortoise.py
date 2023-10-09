fa29f2fcdf3df4846ad0d05ab88af6b6b7da3b61
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2023-10-09 03:15:45 -0400

    Add files via upload

---
+++ VoxNovel+BookNLP(FREE)/run_gui_tortoise.py
@@ -0,0 +1,26 @@
+import subprocess
+def run_script(script_name):
+    """Execute a Python script and wait for it to finish."""
+    result = subprocess.run(["python", script_name], check=True)
+    return result.returncode == 0
+
+def main():
+    scripts_to_run = [
+        "GUINonQuotesCSV.py",
+        "GUIQuotesCSV.py",
+        "cleanup_quotesCSV.py",
+        "create_book_csv.py",
+        "gui_long_tortoise_generate_audio.py",
+        "book_display_and_combine_gui.py"
+    ]
+
+    for script in scripts_to_run:
+        print(f"Running {script} ...")
+        success = run_script(script)
+        if not success:
+            print(f"Error encountered while running {script}. Stopping execution.")
+            break
+        print(f"{script} completed.")
+
+if __name__ == "__main__":
+    main()
